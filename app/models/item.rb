ec5136790d04e1d230bdd37bbd7345f32e85e5ca
Albert J. Rodenius <albertr@mit.edu>
2013-05-08 15:52:59 -0400

    fix categories

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,6 @@
 class Item < ActiveRecord::Base
   attr_accessible :brand, :description, :name, :price, :category
 
-  belongs_to :category
-
   searchable do
     text :name
     text :category
--- db/schema.rb
+++ db/schema.rb
@@ -33,11 +33,11 @@ ActiveRecord::Schema.define(:version => 20130508184308) do
 
   create_table "items", :force => true do |t|
     t.string   "name"
-    t.decimal  "price"
+    t.decimal  "price",       :precision => 8, :scale => 2
     t.text     "description"
     t.string   "brand"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
     t.string   "category"
   end
 
