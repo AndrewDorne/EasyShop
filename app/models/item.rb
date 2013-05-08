e9de7dd94e3d54f524eb85749a9fb88c57d81f03
Albert J. Rodenius <albertr@mit.edu>
2013-05-08 01:04:46 -0400

    center search bar

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,8 @@
 class Item < ActiveRecord::Base
   attr_accessible :brand, :description, :name, :price
 
-<<<<<<< HEAD
   has_and_belongs_to_many :categories
-=======
   searchable do
     text :name
   end
->>>>>>> c7f69c07cfac567859ffc1f0c420e6149e1ea0c9
 end
